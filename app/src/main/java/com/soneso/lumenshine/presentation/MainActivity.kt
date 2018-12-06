3e2a44db28c57e04fbfee6bb9bab5aa001fb7944
Cristi Paval <onisio.software@gmail.com>
2018-12-05 22:44:24 -0500

    Refactoring on navigation in main activity.

--- app/src/main/java/com/soneso/lumenshine/presentation/MainActivity.kt
+++ app/src/main/java/com/soneso/lumenshine/presentation/MainActivity.kt
@@ -5,15 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.GravityCompat
-import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.appbar.AppBarLayout
 import com.soneso.lumenshine.R
 import com.soneso.lumenshine.presentation.general.LsActivity
-import com.soneso.lumenshine.presentation.general.LsFragment
-import com.soneso.lumenshine.presentation.home.HomeFragment
-import com.soneso.lumenshine.presentation.settings.FingerPrintSetupActivity
-import com.soneso.lumenshine.presentation.settings.SettingsFragment
-import com.soneso.lumenshine.presentation.wallets.WalletsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -21,26 +20,20 @@ import kotlinx.android.synthetic.main.app_bar_main.*
 class MainActivity : LsActivity(), com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener {
 
     private var onStopTs = 0L
-    private lateinit var viewModel: MainViewModel
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-
-            com.google.android.material.snackbar.Snackbar.make(view, "No action set on this button!", com.google.android.material.snackbar.Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        setupNavigation()
 
         val toggle = ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        collapsing_toolbar.isTitleEnabled = false
+        collapsingToolbarLayout.isTitleEnabled = false
         changeTitle(R.string.app_name)
 
         drawerView.setNavigationItemSelectedListener(this)
@@ -48,10 +41,30 @@ class MainActivity : LsActivity(), com.google.android.material.navigation.Naviga
         val homeItem = drawerView.menu.getItem(0)
         homeItem.isChecked = true
         onNavigationItemSelected(homeItem)
-        if (intent.hasExtra(EXTRA_FINGERPRINT_SETUP)) {
-            if (intent.getBooleanExtra(EXTRA_FINGERPRINT_SETUP, false))
-                startActivity(Intent(this, FingerPrintSetupActivity::class.java))
+    }
+
+    private fun setupNavigation() {
+        navController = NavHostFragment.findNavController(navHostFragment)
+        navController.addOnNavigatedListener { _, _ ->
+            val destination = navController.currentDestination ?: return@addOnNavigatedListener
+            setupDestination(destination)
         }
+    }
+
+    private fun setupDestination(destination: NavDestination) {
+        when (destination.id) {
+            R.id.home_screen -> setAppBarLocked(false)
+            else -> setAppBarLocked(true)
+        }
+    }
+
+    private fun setAppBarLocked(locked: Boolean) {
+        appBarLayout.setExpanded(!locked)
+        val params = appBarLayout.layoutParams as? CoordinatorLayout.LayoutParams
+        val behavior = params?.behavior as? AppBarLayout.Behavior
+        behavior?.setDragCallback(object : AppBarLayout.Behavior.DragCallback() {
+            override fun canDrag(appBarLayout: AppBarLayout) = !locked
+        })
     }
 
     override fun onBackPressed() {
@@ -83,51 +96,24 @@ class MainActivity : LsActivity(), com.google.android.material.navigation.Naviga
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.home_item -> {
-                fab.show()
-                changeTitle(R.string.app_name)
-                app_bar_layout.setExpanded(true)
-                replaceFragment(HomeFragment.newInstance(), HomeFragment.TAG)
-            }
-            R.id.nav_wallets -> {
-                app_bar_layout.setExpanded(false)
-                replaceFragment(WalletsFragment.newInstance(), WalletsFragment.TAG)
-            }
+            R.id.home_item -> navController.navigate(R.id.to_home_screen)
+            R.id.nav_wallets -> navController.navigate(R.id.wallets_screen)
             R.id.nav_transactions -> {
-
             }
             R.id.nav_currencies -> {
-
             }
             R.id.nav_contacts -> {
-
             }
             R.id.nav_extras -> {
-
             }
-            R.id.nav_settings -> {
-                app_bar_layout.setExpanded(false)
-                changeTitle(R.string.settings)
-                fab.hide()
-                replaceFragment(SettingsFragment.newInstance(), SettingsFragment.TAG)
-            }
+            R.id.nav_settings -> navController.navigate(R.id.to_settings_screen)
             R.id.nav_help -> {
-
             }
             R.id.nav_sign_out -> {
-
             }
         }
-
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
-    }
-
-    private fun replaceFragment(fragment: LsFragment, tag: String) {
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container, fragment, tag)
-                .commit()
     }
 
     private fun changeTitle(titleId: Int) {
@@ -141,13 +127,5 @@ class MainActivity : LsActivity(), com.google.android.material.navigation.Naviga
         fun startInstance(context: Context) {
             context.startActivity(Intent(context, MainActivity::class.java))
         }
-
-        fun startInstanceWithFingerprintSetup(context: Context) {
-            val intent = Intent(context, MainActivity::class.java)
-            intent.putExtra(EXTRA_FINGERPRINT_SETUP, true)
-            context.startActivity(intent)
-        }
-
-        const val EXTRA_FINGERPRINT_SETUP = "fingerpirnt_setup"
     }
 }
--- app/src/main/java/com/soneso/lumenshine/presentation/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.soneso.lumenshine.presentation
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-
-}
